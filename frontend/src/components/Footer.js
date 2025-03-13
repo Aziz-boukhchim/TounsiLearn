0c87ab4071a1c6d1d2c35d6f71490207832204aa
Aziz boukhchim <azizboukhchim321@gmail.com>
2025-03-13 01:52:33 +0100

    Added a footer to the website

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import '../styles/Footer.css';
+
+const Footer = () => {
+
+
+    return(
+        <footer className="footer">
+            <p>&copy; 2025 Aziz Boukhchim. All Rights Reserved.</p>
+            <div className="footer-links">
+            <a href="https://github.com/Aziz-boukhchim" target="_blank">Github</a> | 
+            <a href="https://www.linkedin.com/in/aziz-boukhchim-662722288/" target="_blank">Linkedin</a> | 
+            azizboukhchim321@gmail.com
+            </div>
+
+
+
+        </footer>
+    );
+
+
+
+
+}
+export default Footer;
